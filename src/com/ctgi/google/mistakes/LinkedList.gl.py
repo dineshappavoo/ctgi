b55492b919508d5d7d3cd462f8a8c8fdc9e43d86
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-11-22 23:44:33 -0600

    Added mistakes

---
+++ src/com/ctgi/google/mistakes/LinkedList.gl.py
@@ -0,0 +1,8 @@
+COMMON MISTAKES IN LINKEDLIST:
+
+1. Iterating through the nodes in the linkedlist you may use while loop. Inside the while loop dont forget to move the pointers while(head!=null)
+                    head=head.next
+2. 
+
+
+
