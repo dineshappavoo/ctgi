64437dd8c6df555b8e47b1d7ad95f7177915d329
dinesgappavoo <dinesha.cit@gmail.com>
2015-01-28 02:06:00 -0600

    Added bit manipulation code

---
+++ src/com/ctgi/google/bitmanipulation/BitOperations.java
@@ -0,0 +1,108 @@
+/**
+ * 
+ */
+package com.ctgi.google.bitmanipulation;
+
+/**
+ * @author Dany
+ *
+ */
+public class BitOperations {
+
+	/**
+	 * Method to return the it bit
+	 * @param num
+	 * @param i
+	 * @return
+	 */
+	public int getBit(int num, int i)
+	{
+		
+	}
+	
+	/**
+	 * Method to set the ith bit
+	 * @param num
+	 * @param i
+	 * @return
+	 */
+	public int setBit(int num, int i)
+	{
+		
+	}
+	
+	/**
+	 * Method to clear a bit
+	 * @param num
+	 * @param i
+	 * @return
+	 */
+	public int clearBit(int num, int i)
+	{
+		
+	}
+	
+	/**
+	 * Method to update a bit
+	 * @param num
+	 * @param i
+	 * @return
+	 */
+	public int updateBit(int num, int i)
+	{
+		
+	}
+	
+	/**
+	 * Method to do EX-OR operation
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	public int doExOr(int num1, int num2)
+	{
+		
+	}
+	
+	/**
+	 * Method to do AND operation
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	public int doAnd(int num1, int num2)
+	{
+		
+	}
+	
+	/**
+	 * Method to do OR operation
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	public int doOr(int num1, int num2)
+	{
+		
+	}
+	
+	/**
+	 * Method to do NOT operation
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	public int doNot(int num1, int num2)
+	{
+		
+	}
+		
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
