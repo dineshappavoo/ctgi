b6087ba160a868124a7639ecb13f7206e8f0ca66
dinesgappavoo <dinesha.cit@gmail.com>
2015-04-15 05:38:29 -0500

    Added merge k sorted lists

---
+++ src/com/ctgi/google/problems/Number.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ctgi.google.problems;
+
+/**
+ * @author Dany
+ *
+ */
+public class Number {
+
+	int listIndex;
+	int value;
+	public Number(int listIndex, int value)
+	{
+		this.listIndex = listIndex;
+		this.value = value;
+	}
+}
